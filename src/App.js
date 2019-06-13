dad6bf042f9e83062407b3156e2fa1cb848baf06
Jeffrey Marks <JKDMarks@gmail.com>
2019-06-13 15:05:36 -0400

    RecipesPage and RecipeCard

--- src/App.js
+++ src/App.js
@@ -4,46 +4,18 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import LoginPage from './components/LoginPage'
 import SignupPage from './components/SignupPage'
+import HomePage from './components/HomePage'
+import RecipesPage from './components/RecipesPage'
 
 export default class App extends React.Component {
-  state = {
-    page: "index", // "index", "signup", "login"
-    searchUrl: ""
-  }
-
-  recipeForm = () => (
-    <Form onSubmit={this.createRecipe}>
-      <Form.Field onChange={e => this.setState({searchUrl: e.target.value})}>
-        <label>Recipe</label>
-        <input placeholder='Recipe URL' />
-      </Form.Field>
-      <Button type='submit'>Submit</Button>
-    </Form>
-  )
-
-  createRecipe = () => {
-    // SEND URL TO recipes#create
-    fetch("http://localhost:3000/recipes", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ url: this.state.searchUrl })
-    })
-      .then(r => r.json())
-      .then(data => {
-        debugger
-      })
-  }
-
-
-
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" render={() => <div>Hi welcome home. 🙃</div>}/>
-          <Route path="/login" render={({ history }) => <LoginPage history={history} />}/>
-          <Route path="/signup" render={({ history }) => <SignupPage history={history} />}/>
-          <Route path="/recipes" render={this.recipeForm} />
+          <Route exact path="/" render={({ history }) => <HomePage history={history} /> } />
+          <Route path="/login" render={({ history }) => <LoginPage history={history} />} />
+          <Route path="/signup" render={({ history }) => <SignupPage history={history} />} />
+          <Route path="/recipes" render={() => <RecipesPage />} />
         </Switch>
       </div>
     )
---
+++ src/components/HomePage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="login-logout clearfix" style={{position: "relative"}}>
+          <div style={{float: "left", margin: "0", position: "absolute", top: "50%", transform: "translateY(-50%)"}}>Hi welcome home. 🙃</div>
+          {
+            localStorage.token ?
+            (
+              <Button negative style={{float: "right"}} onClick={() => {delete localStorage.token; this.props.history.push("/")}}>Logout</Button>
+            ) : (
+              <Button positive style={{float: "right"}} onClick={() => this.props.history.push("/login")}>Login</Button>
+            )
+          }
+        </div>
+      </div>
+    )
+  }
+}
