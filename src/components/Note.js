de99ee0a941dcf2f16f84d0134893665b2282c5a
danfyfe <fyfedaniel@gmail.com>
2019-06-14 15:58:03 -0400

    user image

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { Button, Card } from 'semantic-ui-react'
+
 
 export default class Note extends Component {
   render(){
     return(
       <div>
-        Hello from a Note!
+        <Card>
+          <Card.Header>
+            <h4 style={{padding:"10px"}}>Note: </h4>
+          </Card.Header>
+
+          <Card.Content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </Card.Content>
+
+          <Button negative attached="bottom">Delete Note</Button>
+        </Card>
       </div>
     )
   }
